deb48ab708064d8f1a20c20dacab2d2da96bfbc2
saber <365919004@qq.com>
2019-02-28 13:23:58 +0800

    s

--- target/lassm/js/file.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	 /*表单的确认按钮点击事件
-     * 提交表单     */
-    $('div i.fa-check').click(function(){
-        if(check() == true)
-    	   document.forms["fileAndSystemForm"].submit();
-    });
-    
-});
